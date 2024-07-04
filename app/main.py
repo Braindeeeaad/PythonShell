98c0bda35af4b68238a0db7f308a34fcb501eab9
Braindeeeaad <Indrajith2048@gmail.com>
2024-07-04 13:34:45 -0500

    stage5at1

--- app/main.py
+++ app/main.py
@@ -1,5 +1,17 @@
 import sys
 
+def handle_response(line) -> bool:
+    command = line.split(" ")
+    match(command[0]):
+        case "exit":
+            if command[1]=="0":
+                return False
+        case "echo":
+            sys.stdout.write(f"{command[1]}\n")
+            return True
+
+    sys.stdout.write(f"{line}: command not found\n")
+    return True
 def repl():
     while True:
         sys.stdout.write("$ ")
@@ -7,10 +19,9 @@ def repl():
 
         # Wait for user input
         line = input()
-        command = line.split(" ")
-        if(command[0]=="exit" and command[1]=="0"):
+        if not handle_response(line):
             return
-        sys.stdout.write(f"{line}: command not found\n")
+
 
 
 def main():

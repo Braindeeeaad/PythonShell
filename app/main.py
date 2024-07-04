2f9af88c30029da6672cd151155e92d8491e3d26
Braindeeeaad <Indrajith2048@gmail.com>
2024-07-04 10:04:52 -0500

    Stage1At1

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ import sys
 
 def main():
     # Uncomment this block to pass the first stage
-    # sys.stdout.write("$ ")
-    # sys.stdout.flush()
+     sys.stdout.write("$ ")
+     sys.stdout.flush()
 
     # Wait for user input
-    input()
+     input()
 
 
 if __name__ == "__main__":
